c0840f96a9ff13fb10d836e02dc1177281e8ace9
Samuel Powell <samuelpowell@noreply.users.github.com>
2017-02-05 16:46:25 +0000

    Add basic mesh tests

---
+++ test/mesh.jl
@@ -0,0 +1,23 @@
+# libtoast.jl: interface to the TOAST++ library
+# Copyright (C) 2017 Samuel Powell
+
+meshdir_2D = joinpath(libtoast._jl_toast_dir, "test", "2D", "meshes")
+meshdir_3D = joinpath(libtoast._jl_toast_dir, "test", "3D", "meshes")
+
+@testset "Meshes" begin
+
+  m = Mesh(joinpath(meshdir_2D, "circle25_32.msh"));
+  @test numnodes(m) == 3511
+  @test numelems(m) == 6840
+  @test numdims(m) == 2
+  @test boundingbox(m) == [-25.0 -25.0; 25.0 25.0]
+  @test fullsize(m) == 1963.0959349692218
+
+  m = Mesh(joinpath(meshdir_3D, "cyl3.msh"));
+  @test numnodes(m) == 27084
+  @test numelems(m) == 141702
+  @test numdims(m) == 3
+  @test boundingbox(m) == [-25.0 25.0; 25.0 -25.0; -25.0 25.0]
+  @test fullsize(m) == 98056.71142861326
+
+end
