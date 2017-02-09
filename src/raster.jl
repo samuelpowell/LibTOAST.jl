13421553a6f7ba6933f3043e498d4e31b7509714
Samuel Powell <samuelpowell@users.noreply.github.com>
2017-02-09 21:15:26 +0000

    WIP raster types

--- src/raster.jl
+++ src/raster.jl
@@ -5,113 +5,93 @@
 import Base: string, print, show
 
 # Export types
-export PixelBasis
+export PixelRaster
 
 # Export methods
 export map, gradient
 
-# Type definitions
-abstract Raster
+@enum RasterShapes Pixel CubicPixel GaussBlob BesselBlob HanningBlob RampBlob SplineBlob
 
-type PixelBasis <: Raster
+# Type definitions
+type Raster{S<:RasterShapes}
 
   ptr::Cxx.CppPtr
   mesh::Mesh
-  #
-  function PixelBasis(mesh::Mesh, bdim::Vector{Cint})
-  #
-  #   gdim = 2*bdim
-  #   bb = boundingbobx(mesh)
-  #
-  #   ptr = @cxxnew Raster_Pixel
-  #   icxx"""$(ptr) = Raster_Pixel ($(pointer(bdim)), $(pointer(gdim)), mesh.ptr, $(pointer(bb)));"""
-  #   raster = new(ptr, mesh)
-  #   finalizer(raster, _raster_delete)
-  #
-  #   return raster
-  #
+
+  function Raster{T<:Integer}(shape::RasterShapes, mesh::Mesh, rdim::Vector{T}; gscale=2)
+
+    dim = dimensions(mesh)
+    assert(length(rdim)==dim)
+    meshptr = mesh.ptr
+    rdimptr = pointer(rdim)
+
+    ptr = icxx"""
+
+      RDenseMatrix *bb = 0;
+
+      IVector bdim($(dim));
+      for (int i = 0; i < $(dim); i++)
+          bdim[i] = $(rdimptr)[i];
+
+      IVector gdim($(dim));
+      for (int i=0; i < $(dim); i++)
+          gdim[i] = $(gscale)*bdim[i];
+
+      Raster *raster;
+
+      switch($(Int(shape))) {
+        case 1:
+          raster = new Raster_CubicPixel (bdim, gdim, mesh, bb);
+          break;
+        case 2:
+          raster = new Raster_GaussBlob (bdim, gdim, mesh, blobarg, blobrad, bb);
+          break;
+        case 3:
+          raster = new Raster_BesselBlob (bdim, gdim, mesh, blobarg, blobrad, bb);
+          break;
+        case 4:
+          raster = new Raster_HanningBlob (bdim, gdim, mesh, blobrad, bb);
+          break;
+        case 5:
+          raster = new Raster_RampBlob (bdim, gdim, mesh, blobrad, bb);
+          break;
+        case 6:
+          raster = new Raster_SplineBlob (bdim, gdim, mesh, blobrad, bb);
+          break;
+      }
+
+      return raster;
+
+    """
+    raster = new{shape}(ptr, mesh)
+    finalizer(raster, _raster_delete)
+
+    return raster
+
   end
 
 end
 
 _raster_delete(raster::Raster) = finalize(raster.ptr);
 
+nlen(raster::Raster) = nodecount(raster.mesh)
+slen(raster::Raster) = @cxx raster.ptr->SLen()
+blen(raster::Raster) = @cxx raster.ptr->Blen()
+glen(raster::Raster) = @cxx raster.ptr->Glen()
 
 
-
-
-
 #
 #
-# // raster_new: create a new raster
-# Raster * raster_new(Mesh *mesh, int *rdim, int dim, int rtyp, int gscale)
-# {
-#     int i;
-#     RDenseMatrix *bb = 0;
-#     double blobrad = 1.0;
-#     double blobarg = 1.0;
-#     double maptol = 1e-10;
-#     double dgscale = 0.1;
+# function map(r::Raster, v::Raster, ::rbasis)
+#   ovec = vec
+#   map!(r::Rater, v::Raster, o::Raster)
+# end
 #
-#     // Copy basis dimensions, set default gdim accordingly
-#     IVector bdim(dim);
-#     for (i = 0; i < dim; i++)
-#         bdim[i] = rdim[i];
+# function map!(r::Raster, v::Raster, ::rbasis)
+#   icxx"""
+#     RVector iprm(ilen, ivec, SHALLOW_COPY);
+#     RVector oprm(olen, ovec, SHALLOW_COPY);
+#     raster->Map_MeshToBasis(iprm, oprm);
+#   """
+# end
 #
-#     IVector gdim(dim);
-#     for (i=0; i <dim; i++)
-#         gdim[i] = gscale*bdim[i];
-#
-#     // Instantiate raster
-#     Raster *raster = 0;
-#     switch (rtyp) {
-#         case 0:
-#             raster = new Raster_Pixel (bdim, gdim, mesh, bb);
-#             break;
-#         case 1:
-#             raster = new Raster_CubicPixel (bdim, gdim, mesh, bb);
-#             break;
-#         case 2:
-#             raster = new Raster_GaussBlob (bdim, gdim, mesh, blobarg, blobrad, bb);
-#             break;
-#         case 3:
-#             raster = new Raster_BesselBlob (bdim, gdim, mesh, blobarg, blobrad, bb);
-#             break;
-#         case 4:
-#             raster = new Raster_HanningBlob (bdim, gdim, mesh, blobrad, bb);
-#             break;
-#         case 5:
-#             raster = new Raster_RampBlob (bdim, gdim, mesh, blobrad, bb);
-#             break;
-#         case 6:
-#             raster = new Raster_SplineBlob (bdim, gdim, mesh, blobrad, bb);
-#             break;
-#         case 7:
-#             raster = Raster2::Create<Raster_Pixel2> (bdim, bdim, mesh, bb, maptol);
-#             break;
-#         case 8:
-#             raster = Raster2::Create<Raster_CPixel> (bdim, bdim, mesh, bb, maptol);
-#             break;
-#         case 9:
-#             raster = Raster_Blob2::Create<Raster_Blob2_RB> (bdim, bdim, mesh, blobrad, blobarg, dgscale, bb, maptol);
-#             break;
-#         case 10:
-#             raster = Raster_Blob2::Create<Raster_Blob2_BB> (bdim, bdim, mesh, blobrad, blobarg, dgscale, bb, maptol);
-#             break;
-#         case 11:
-#             raster = Raster_Blob2::Create<Raster_Blob2_SB> (bdim, bdim, mesh, blobrad, blobarg, dgscale, bb, maptol);
-#             break;
-#         case 12:
-#             raster = Raster_Blob2::Create<Raster_Blob2_HB> (bdim, bdim, mesh, blobrad, blobarg, dgscale, bb, maptol);
-#             break;
-#         case 13:
-#             raster = Raster_Blob2::Create<Raster_Blob2_GB> (bdim, bdim, mesh, blobrad, blobarg, dgscale, bb, maptol);
-#             break;
-#         case 14:
-#             raster = Raster2::Create<Raster_CPixel_Tree> (bdim, bdim, mesh, bb, maptol);
-#             break;
-#     }
-#
-#     if (bb) delete bb;
-#     return raster;
-# }
