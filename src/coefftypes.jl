7584884558b24a1a72576d516db56d98a78680f9
Samuel Powell <samuelpowell@users.noreply.github.com>
2017-02-11 21:20:37 +0000

    WIP coefficient types

--- src/coefftypes.jl
+++ src/coefftypes.jl
@@ -1,8 +1,11 @@
 # TOAST.jl: interface to the TOAST++ library
 # Copyright (C) 2017 Samuel Powell
 
+# Import
+import Base: convert, size, linearindexing, getindex, setindex!
+
 # Export types
-export RasterBases NodalCoeff SolutionCoeff RasterCoeff IntermediateCoeff
+export RasterBases, NodalCoeff, SolutionCoeff, RasterCoeff, IntermediateCoeff
 
 @enum MapTransforms _nb _ng _ns _bn _bg _bs _sn _sb _sg _gn _gs _gb
 
@@ -16,19 +19,20 @@ type NodalCoeff <: AbstractArray{Float64, 1}
   data::Vector{Float64}
   function NodalCoeff(mesh::Mesh, data::Vector{Float64})
     assert(length(data) == nodecount(mesh))
+    return new(mesh,data)
   end
 end
 
-NodalCoeff(mesh::Mesh) = NodalCoeff(mesh, Vector{Float64}(nodecount(mesh)))
-
-Base.size(coeff::NodalCoeff) = nodecount(coeff.mesh)
 Base.linearindexing{T<:NodalCoeff}(::Type{T}) = Base.LinearFast()
-Base.getindex(coeff::NodalCoeff, i::Int) = coeff.data[i]
+size(coeff::NodalCoeff) = (length(coeff.data),)
+getindex(coeff::NodalCoeff, i::Int) = coeff.data[i]
+setindex!(coeff::NodalCoeff, v, i::Int) = (coeff.data[i] = v)
 
 # Raster coefficients represent functions in one of the raster bases
 abstract RasterCoeffTypes <: AbstractArray{Float64, 1}
-Base.linearindexing{T<:RasterCoeff}(::Type{T}) = Base.LinearFast()
-Base.getindex(coeff::RasterCoeff, i::Int) = coeff.data[i]
+Base.linearindexing{T<:RasterCoeffTypes}(::Type{T}) = Base.LinearFast()
+getindex{T<:RasterCoeffTypes}(coeff::T, i::Int) = coeff.data[i]
+setindex!{T<:RasterCoeffTypes}(coeff::T, v, i::Int) = (coeff.data[i] = v)
 
 # Solution coefficients represent a function expressed in the solution basis,
 # which does not include raster points which fall outside of the support of the
@@ -38,11 +42,12 @@ type SolutionCoeff <: RasterCoeffTypes
   data::Vector{Float64}
   function SolutionCoeff(rmap::RasterMap, data::Vector{Float64})
     assert(length(data) == slen(rmap))
+    return new(rmap,data)
   end
 end
 
 SolutionCoeff(rmap::RasterMap) = SolutionCoeff(rmap, Vector{Float64}(slen(rmap)))
-Base.size(coeff::SolutionCoeff) = slen(coeff.rmap)
+size(coeff::SolutionCoeff) = (slen(coeff.rmap),)
 
 # Raster coefficients represent a function expressed in the rasterised basis,
 # which is defined over a square or cuboid redion, and may include superfluous
@@ -52,11 +57,12 @@ type RasterCoeff <: RasterCoeffTypes
   data::Vector{Float64}
   function RasterCoeff(rmap::RasterMap, data::Vector{Float64})
     assert(length(data) == blen(rmap))
+    return new(rmap,data)
   end
 end
 
 RasterCoeff(rmap::RasterMap) = RasterCoeff(rmap, Vector{Float64}(blen(rmap)))
-Base.size(coeff::RasterCoeff) = blen(coeff.rmap)
+size(coeff::RasterCoeff) = (blen(coeff.rmap),)
 
 # Intermediate coefficients represent a function expressed in a higher resolution
 # version of the raster basis, and may include superfluous elements which are
@@ -64,68 +70,62 @@ Base.size(coeff::RasterCoeff) = blen(coeff.rmap)
 type IntermediateCoeff <: RasterCoeffTypes
   rmap::RasterMap
   data::Vector{Float64}
-  function RasterCoeff(rmap::RasterMap, data::Vector{Float64})
+  function IntermediateCoeff(rmap::RasterMap, data::Vector{Float64})
     assert(length(data) == glen(rmap))
+    return new(rmap,data)
   end
 end
 
 IntermediateCoeff(rmap::RasterMap) = IntermediateCoeff(rmap, Vector{Float64}(glen(rmap)))
-Base.size(coeff::IntermediateCoeff) = glen(coeff.rmap)
+size(coeff::IntermediateCoeff) = (glen(coeff.rmap),)
 
 #
 # Coefficient mapping (and construction)
 #
 
 # Convert everything to nodal coefficients
-function convert{T<:RasterCoeffTypes}(NodalCoeff, ci::T)
+function convert{T<:RasterCoeffTypes}(::Type{NodalCoeff}, ci::T)
   co = NodalCoeff(ci.rmap)
   map!(co, ci)
   return co
 end
 
 map!(co::NodalCoeff, ci::SolutionCoeff)  = _map!(co.data, ci.data, co.rmap, _ns)
-map!(co::NodalCoeff, ci::RasterCoeff) = _map!((co.data, ci.data, co.rmap, _nb)
+map!(co::NodalCoeff, ci::RasterCoeff) = _map!(co.data, ci.data, co.rmap, _nb)
 map!(co::NodalCoeff, ci::IntermediateCoeff) = _map!(co.data, ci.data, co.rmap, _ng)
 
 # Convert everything to raster coefficients
-function convert{T<:Union{RasterCoeffTypes, NodalCoeff}}(RasterCoeff, ci::T)
+function convert{T<:RasterCoeffTypes}(::Type{RasterCoeff}, ci::T)
   co = RasterCoeffs(ci.rmap)
   map!(co,ci)
   return co
 end
 
 map!(co::RasterCoeff, ci::SolutionCoeff)  = _map!(co.data, ci.data, co.rmap, _sb)
-map!(co::RasterCoeff, ci::NodalCoeff) = _map!((co.data, ci.data, co.rmap, _nb)
+map!(co::RasterCoeff, ci::NodalCoeff) = _map!(co.data, ci.data, co.rmap, _nb)
 map!(co::RasterCoeff, ci::IntermediateCoeff) = _map!(co.data, ci.data, co.rmap, _gb)
 
 # Convert everything to solution coefficients
-function convert{T<:Union{RasterCoeffTypes, NodalCoeff}}(SolutionCoeff, ci::T)
+function convert{T<:RasterCoeffTypes}(::Type{SolutionCoeff}, ci::T)
   co = SolutionCoeff(ci.rmap)
   map!(co,ci)
   return co
 end
 
 map!(co::SolutionCoeff, ci::RasterCoeff)  = _map!(co.data, ci.data, co.rmap, _bs)
-map!(co::SolutionCoeff, ci::NodalCoeff) = _map!((co.data, ci.data, co.rmap, _ns)
+map!(co::SolutionCoeff, ci::NodalCoeff) = _map!(co.data, ci.data, co.rmap, _ns)
 map!(co::SolutionCoeff, ci::IntermediateCoeff) = _map!(co.data, ci.data, co.rmap, _gs)
 
 # Convert everything to intermediate coefficients
-function convert{T<:Union{RasterCoeffTypes, NodalCoeff}}(IntermediateCoeff, ci::T)
+function convert{T<:RasterCoeffTypes}(::Type{IntermediateCoeff}, ci::T)
   co = IntermediateCoeff(ci.rmap)
   map!(co,ci)
   return co
 end
 
 map!(co::IntermediateCoeff, ci::RasterCoeff)  = _map!(co.data, ci.data, co.rmap, _bg)
-map!(co::IntermediateCoeff, ci::NodalCoeff) = _map!((co.data, ci.data, co.rmap, _ng)
+map!(co::IntermediateCoeff, ci::NodalCoeff) = _map!(co.data, ci.data, co.rmap, _ng)
 map!(co::IntermediateCoeff, ci::SolutionCoeff) = _map!(co.data, ci.data, co.rmap, _sg)
-
-
-
-
-
-void raster_map(Raster *raster, double *ivec, int ilen, double *ovec, int olen, int mode)
-
 
 # Low level mapping function
 function _map!(ovec::Vector{Float64},
