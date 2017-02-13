05732b1e265962c956ccc31a8ea818a7d6a7e5c1
Samuel Powell <s.powell@ucl.ac.uk>
2017-02-13 14:51:08 +0000

    Load meshes once after test, fix #10

--- test/rastermap.jl
+++ test/rastermap.jl
@@ -3,20 +3,18 @@
 
 @testset "RasterMap" begin
 
-  mesh = Mesh(joinpath(meshdir_2D, "circle25_32.msh"));
-
   @testset "PixelMap" begin
 
     nx, ny = 20, 20
-    pixelmap = PixelMap(mesh, (nx,ny))
-    @test nlen(pixelmap) == nodecount(mesh)
+    pixelmap = PixelMap(mesh2D, (nx,ny))
+    @test nlen(pixelmap) == nodecount(mesh2D)
     @test blen(pixelmap) == nx*ny
     @test glen(pixelmap) == nx*ny*2*2
     @test slen(pixelmap) == 352
 
     gscale = 4
-    pixelmap = PixelMap(mesh, (nx,ny), gscale=gscale)
-    @test nlen(pixelmap) == nodecount(mesh)
+    pixelmap = PixelMap(mesh2D, (nx,ny), gscale=gscale)
+    @test nlen(pixelmap) == nodecount(mesh2D)
     @test blen(pixelmap) == nx*ny
     @test glen(pixelmap) == nx*ny*gscale*gscale
     @test slen(pixelmap) == 352
--- test/runtests.jl
+++ test/runtests.jl
@@ -9,6 +9,10 @@ meshdir_3D = joinpath(TOAST._jl_toast_dir, "test", "3D", "meshes")
 @testset "TOAST.jl Core" begin
 
   include("mesh.jl")
+
+  global mesh2D = Mesh(joinpath(meshdir_2D, "circle25_32.msh"));
+  global mesh3D = Mesh(joinpath(meshdir_2D, "circle25_32.msh"));
+
   include("sysmat.jl")
   include("assembly.jl")
   include("rastermap.jl")
